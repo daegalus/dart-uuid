bcc5494d39624db408e2dc4dde729ea20f033410
Yulian Kuncheff <yulian@kuncheff.com>
2012-09-05 00:13:19 -0700

    UUIDv5, fixed 2 bugs with UUIDv4, and added new unit tests.

--- tests/testUuid.dart
+++ tests/testUuid.dart
@@ -6,6 +6,20 @@ main() {
   var uuid = new Uuid();
   final int TIME = 1321644961388;
 
+  List mathRNGCustom() {
+    var rand, b = new List(16);
+
+    for(var i = 0; i < 16; i++) {
+      if ((i & 0x03) == 0) {
+        var _rand = new Math.Random(1);
+        rand = (_rand.nextDouble() * 0x100000000).floor().toInt();
+      }
+      b[i] = rand >> ((i & 0x03) << 3) & 0xff;
+    }
+
+    return b;
+  }
+  
   group('[Version 1 Tests]', () {
     test('IDs created at same mSec are different', () {
       expect(uuid.v1({'mSecs': TIME}), isNot(equals(uuid.v1({'mSecs': TIME}))));
@@ -26,7 +40,7 @@ main() {
       var uidtb = uuid.v1({'mSecs': TIME - 1});
 
       expect((Math.parseInt("0x${uidtb.split('-')[3]}")
-          - Math.parseInt("0x${uidt.split('-')[3]}")), equals(1));
+          - Math.parseInt("0x${uidt.split('-')[3]}")), anyOf(equals(1), equals(-16383)));
     });
 
     test('Clock regression by msec increments the clockseq - nSec', () {
@@ -58,7 +72,44 @@ main() {
       expect(dt, equals(1));
     });
   });
-
+  
+  group('[Version 4 Tests]', () {
+    test('Check if V4 is consistent using a static seed', () {
+      var u0 = uuid.v4({
+        'rng': mathRNGCustom()
+      });
+      var u1 = "0fc6a4b0-0fc6-44b0-8fc6-a4b00fc6a4b0";
+      expect(u0, equals(u1));
+    });
+    
+    test('Return same output as entered for "random" option', () {
+      var u0 = uuid.v4({
+        'random': [
+                   0x10, 0x91, 0x56, 0xbe, 0xc4, 0xfb, 0xc1, 0xea,
+                   0x71, 0xb4, 0xef, 0xe1, 0x67, 0x1c, 0x58, 0x36
+                   ]
+      });
+      var u1 = "109156be-c4fb-41ea-b1b4-efe1671c5836";
+      expect(u0, equals(u1));
+    });
+  });
+  
+  group('[Version 5 Tests]', () {
+    test('Using URL namespace and custom name', () {
+      var u0 = uuid.v5(uuid.NAMESPACE_URL, 'www.google.com');
+      var u1 = uuid.v5(uuid.NAMESPACE_URL, 'www.google.com');
+      
+      expect(u0, equals(u1));
+    });
+    
+    test('Using Random namespace and custom name', () {
+      var u0 = uuid.v5(null, 'www.google.com');
+      var u1 = uuid.v5(null, 'www.google.com');
+      
+      expect(u0, isNot(equals(u1)));
+    });
+  });
+  
   group('[Parse/Unparse Tests]', () {
     test('Parsing a short/cut-off UUID', () {
       var id = '00112233445566778899aabbccddeeff';
